7e8e4f0b786abc88c7b48072dbbb9dd6c5b5cf2f
Ivan Nikolov <ivannikolov007@gmail.com>
2016-01-13 18:26:58 +0200

    finale

--- work/connect.cpp
+++ work/connect.cpp
@@ -27,15 +27,6 @@ int main(int argc, char *argv[]){
 
     char cmd[100];
 
-    std::string ip;
-    ip = exec("ifconfig | grep -A1 -E 'tether|wlan0' | grep -Eo 'inet (addr:)?([0-9]*\\.){3}[0-9]*' | grep -Eo '([0-9]*\\.){3}[0-9]*' | grep -v '127.0.0.1'");
-    ip.erase(10,4);
-    ip.append("1 ");
-    ip.insert(0, "./client ");
-    ip.append(std::string(argv[1]));
-
-    char cmd[100];
-
     strcpy(cmd, ip.c_str());
 
     std::cout<<cmd<<std::endl;
